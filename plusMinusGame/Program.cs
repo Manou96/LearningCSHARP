3d8490bb62a1c7c4a194c2a07b539bbb3e4e3d1f
Manou96 <esarr33@gmail.com>
2019-03-10 18:53:41 -0400

    plus minus

--- plusMinusGame/Program.cs
+++ plusMinusGame/Program.cs
@@ -5,9 +5,36 @@ namespace plusMinusGame
     class Program
     {
         static void Main(string[] args)
-        {
+        {   int valeurATrouver = new Random().Next(0, 100);
+            int nbCoup=0;
+            bool gagnant=false;
+            string input;
+            int valeur;
+
             Console.WriteLine("================================PLUS MINUS GAME================================");
-            Console.WriteLine("Choisissez un entier entre 1 et 100");
+            Console.WriteLine("Cherchez le nombre magique entre 1 et 100");
+            do{    
+            input=Console.ReadLine();
+            if(int.TryParse(input, out valeur)){
+            
+                if(valeur<valeurATrouver){
+                Console.WriteLine("Plus Grand...");
+                nbCoup++;
+                }else if(valeur>valeurATrouver){
+                Console.WriteLine("Plus Petit...");
+                nbCoup++;
+                }else{
+                Console.WriteLine("================================VOUS AVEZ TROUVE================================");
+                Console.WriteLine("Vous avez trouvé en "
+                +nbCoup+" coup(s)");
+                gagnant=true;    
+                }
+
+            }else{
+                
+                Console.WriteLine("La valeur saisie est incorrecte");
+            }
+            }while(!gagnant);
         }
     }
 }
