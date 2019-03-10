c518ac1312caeebdc0df8a1526f038e5a83e88b1
Manou96 <esarr33@gmail.com>
2019-03-10 17:02:13 -0400

    loto code

--- lotoTirage/Program.cs
+++ lotoTirage/Program.cs
@@ -6,7 +6,43 @@ namespace lotoTirage
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            Console.WriteLine("Tirage loto");
+            int[] tirage=new int[7];
+            tirageAlea(tirage);
+            ecrireTirage(tirage);
+        }
+
+        static void tirageAlea(int[] tab){
+            Random alea=new Random();
+            for(int i=0;i<tab.Length;i++){
+
+            int randomNumber= alea.Next(1,49);
+                while(isTaken(tab,i,randomNumber)){
+                randomNumber=alea.Next(1,49);
+                }
+            tab[i]=randomNumber;    
+            }
+
+        }
+
+        static bool isTaken(int[] tab, int pos, int number){
+            bool isTaken=false;
+
+            if(pos==0){
+                isTaken=false;
+            }else{
+                for(int i=0;i<pos;i++){
+                    if(number==tab[i]){isTaken=true;}
+                }
+            }
+            return isTaken;
+        }
+
+        static void ecrireTirage(int[] tab){
+            for(int i=0;i<tab.Length;i++){
+                Console.WriteLine(tab[i]);
+            }
         }
     }
 }
